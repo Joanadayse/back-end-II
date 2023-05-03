1a50c997867daa6318a4bca8d53c928b5b193031
Joana Dayse <101282171+Joanadayse@users.noreply.github.com>
2023-05-02 21:14:58 -0300

    pratica-poo-iii

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
-import { TAccountDB, TAccountDBPost, TUserDB, TUserDBPost } from './types'
-import { db } from './database/knex'
+import { AccountDB, UserDB } from './types'
 import { User } from './models/User'
 import { Account } from './models/Account'
+import { UserDatabase } from './database/UserDatabase'
+import { AccountDatabase } from './database/AccountDatabase'
+import { UserController } from './controller/UserController'
+import { AccountController} from './controller/AccountController'
 
 const app = express()
 
@@ -13,6 +16,11 @@ app.use(express.json())
 app.listen(3003, () => {
     console.log(`Servidor rodando na porta ${3003}`)
 })
+
+//-------------------------- instancias 
+const userController= new UserController()
+const accountController= new AccountController()
+// -------------------------
 
 app.get("/ping", async (req: Request, res: Response) => {
     try {
@@ -32,275 +40,13 @@ app.get("/ping", async (req: Request, res: Response) => {
     }
 })
 
-app.get("/users", async (req: Request, res: Response) => {
-    try {
-        const q = req.query.q
 
-        let usersDB
+//-------------------------- users
+app.get("/users",userController.getUsers)
+app.post("/users" , userController.createUsers)
 
-        if (q) {
-            const result: TUserDB[] = await db("users").where("name", "LIKE", `%${q}%`)
-            usersDB = result
-        } else {
-            const result: TUserDB[] = await db("users")
-            usersDB = result
-        }
-            const users:User[]= usersDB.map((user)=>{
-                return new User(
-                    user.id,
-                    user.name,
-                    user.email,
-                    user.password,
-                    user.created_at,
-                )
-      
-    })
-
-        // console.log(users[0].getName())
-        res.status(200).send(users)
-    
-    } catch (error) {
-        console.log(error)
-
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
-
-app.post("/users", async (req: Request, res: Response) => {
-    try {
-        const { id, name, email, password } = req.body
-
-        if (typeof id !== "string") {
-            res.status(400)
-            throw new Error("'id' deve ser string")
-        }
-
-        if (typeof name !== "string") {
-            res.status(400)
-            throw new Error("'name' deve ser string")
-        }
-
-        if (typeof email !== "string") {
-            res.status(400)
-            throw new Error("'email' deve ser string")
-        }
-
-        if (typeof password !== "string") {
-            res.status(400)
-            throw new Error("'password' deve ser string")
-        }
-
-        const [ userDBExists ]: TUserDB[] | undefined[] = await db("users").where({ id })
-
-        if (userDBExists) {
-            res.status(400)
-            throw new Error("'id' já existe")
-        }
-
-        // const newUser: TUserDBPost = {
-        //     id,
-        //     name,
-        //     email,
-        //     password
-        // }
-        const newUser= new User(
-            id,
-            name,
-            email,
-            password,
-            new Date().toISOString()
-        )
-        const newUserDB: TUserDB={
-            id: newUser.getId(),
-            name: newUser.getName(),
-            email: newUser.getEmail(),
-            password: newUser.getPassword(),
-            created_at: newUser.getCreateAt()
-        }
-        
-// console.log(newUser)
-        await db("users").insert(newUserDB)
-        const [ userDB ]: TUserDB[] = await db("users").where({ id })
-
-        res.status(201).send(userDB)
-    } catch (error) {
-        console.log(error)
-
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
-
-app.get("/accounts", async (req: Request, res: Response) => {
-    try {
-        const accountsDB: TAccountDB[] = await db("accounts")
-
-        res.status(200).send(accountsDB)
-    } catch (error) {
-        console.log(error)
-
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
-
-app.get("/accounts/:id/balance", async (req: Request, res: Response) => {
-    try {
-        const id = req.params.id
-
-        const [ accountDB ]: TAccountDB[] | undefined[] = await db("accounts").where({ id })
-
-        if (!accountDB) {
-            res.status(404)
-            throw new Error("'id' não encontrado")
-        }
-
-        const account: Account = new Account(
-            accountDB.id, 
-            accountDB.owner_id, 
-            accountDB.balance, 
-            accountDB.created_at
-        )
-
-        res.status(200).send({ balance: account.getBalance() })
-    } catch (error) {
-        console.log(error)
-
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
-
-
-app.post("/accounts", async (req: Request, res: Response) => {
-    try {
-        const { id, ownerId } = req.body
-
-        if (typeof id !== "string") {
-            res.status(400)
-            throw new Error("'id' deve ser string")
-        }
-
-        if (typeof ownerId !== "string") {
-            res.status(400)
-            throw new Error("'ownerId' deve ser string")
-        }
-
-        const [ accountDBExists ]: TAccountDB[] | undefined[] = await db("accounts").where({ id })
-
-        if (accountDBExists) {
-            res.status(400)
-            throw new Error("'id' já existe")
-        }
-
-        // const newAccount: TAccountDBPost = {
-        //     id,
-        //     owner_id: ownerId
-        // }
-
-        const newAccount: Account = new Account(
-            id,
-            ownerId,
-            0,
-            new Date().toISOString()
-        )
-
-        const newAccountDb: TAccountDBPost={
-            id: newAccount.getId(),
-            owner_id: newAccount.getOwnerId()
-
-        }
-
-        await db("accounts").insert(newAccountDb)
-        const [ accountDB ]: TAccountDB[] = await db("accounts").where({ id })
-
-        res.status(201).send(accountDB)
-    } catch (error) {
-        console.log(error)
-
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
-
-app.put("/accounts/:id/balance", async (req: Request, res: Response) => {
-    try {
-        const id = req.params.id
-        const value = req.body.value
-
-        if (typeof value !== "number") {
-            res.status(400)
-            throw new Error("'value' deve ser number")
-        }
-
-        const [ accountDB ]: TAccountDB[] | undefined[] = await db("accounts").where({ id })
-
-        if (!accountDB) {
-            res.status(404)
-            throw new Error("'id' não encontrado")
-        }
-
-        // accountDB.balance += value
-
-        const account: Account= new Account(
-            accountDB.id,
-            accountDB.owner_id,
-            accountDB.balance,
-            accountDB.created_at
-        )
-
-        account.setBalance(value)
-
-        await db("accounts").update({ balance: account.getBalance() }).where({ id })
-        
-        res.status(200).send(accountDB)
-    } catch (error) {
-        console.log(error)
-
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
+//-------------------------- accounts
+app.get("/accounts", accountController.getAccount)
+app.get("/accounts/:id/balance",accountController.getById)
+app.post("/accounts",accountController.createAccount)
+app.put("/accounts/:id/balance", accountController.editeAccount)
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,25 +1,49 @@
-export class User{
+export class User {    
     constructor(
         private id: string,
         private name: string,
         private email: string,
         private password: string,
-        private create_at: string
-    ){}
+        private createdAt: string
+    ) {}
 
-    public getId():string{
+    public getId(): string {
         return this.id
     }
-    public getName():string{
+    
+    public setId(value: string): void {
+        this.id = value
+    }
+
+    public getName(): string {
         return this.name
     }
-    public getEmail():string{
+
+    public setName(value: string): void {
+        this.name = value
+    }
+
+    public getEmail(): string {
         return this.email
     }
-    public getPassword():string{
+
+    public setEmail(value: string): void {
+        this.email = value
+    }
+
+    public getPassword(): string {
         return this.password
     }
-    public getCreateAt():string{
-        return this.create_at
+
+    public setPassword(value: string): void {
+        this.password = value
+    }
+
+    public getCreatedAt(): string {
+        return this.createdAt
+    }
+
+    public setCreatedAt(value: string): void {
+        this.createdAt = value
     }
 }
