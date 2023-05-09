35d05d66a38752a35028eccc2b054f44a8ffbcb5
Joanadayse <daysejoana3@gmail.com>
2023-05-08 21:40:22 -0300

    arquitetura-I-pratica

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import cors from 'cors'
-import { AccountDB, UserDB } from './types'
-import { User } from './models/User'
-import { Account } from './models/Account'
-import { UserDatabase } from './database/UserDatabase'
-import { AccountDatabase } from './database/AccountDatabase'
 import { UserController } from './controller/UserController'
-import { AccountController} from './controller/AccountController'
+import { AccountController } from './controller/AccountController'
 
 const app = express()
 
@@ -17,36 +12,13 @@ app.listen(3003, () => {
     console.log(`Servidor rodando na porta ${3003}`)
 })
 
-//-------------------------- instancias 
-const userController= new UserController()
-const accountController= new AccountController()
-// -------------------------
+const userController = new UserController()
+const accountController = new AccountController()
 
-app.get("/ping", async (req: Request, res: Response) => {
-    try {
-        res.status(200).send({ message: "Pong!" })
-    } catch (error) {
-        console.log(error)
+app.get("/users", userController.getUsers)
+app.post("/users", userController.createUser)
 
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
-
-
-//-------------------------- users
-app.get("/users",userController.getUsers)
-app.post("/users" , userController.createUsers)
-
-//-------------------------- accounts
-app.get("/accounts", accountController.getAccount)
-app.get("/accounts/:id/balance",accountController.getById)
-app.post("/accounts",accountController.createAccount)
-app.put("/accounts/:id/balance", accountController.editeAccount)
+app.get("/accounts", accountController.getAccounts)
+app.get("/accounts/:id/balance", accountController.getAccountBalance)
+app.post("/accounts", accountController.createAccount)
+app.put("/accounts/:id/balance", accountController.editAccountBalance)
